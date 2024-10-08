16123cc677c60e9838396df9f192233e19fa38ab
Emily Heideman <emilyheideman@campus-038-014.ucdavis.edu>
2024-10-08 14:22:48 -0700

    week 2 homework 10/8/24

---
+++ week_2_homework.R
@@ -0,0 +1,14 @@
+set.seed(15)
+hw2 <- runif(50,4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+na.omit(hw2)
+hw2 <- 14:38
+prob1 <- hw2
+rm(hw2)
+prob1
+prob1*3
+times3 <- prob1*3
+times3 + 10
+plus10 <- times3+10
+final <- plus10[c(TRUE,FALSE)]
