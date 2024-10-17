c6111e80aecf8c04406c05087ece61bb2729be5d
Emily Heideman <emilyheideman@campus-009-214.ucdavis.edu>
2024-10-17 14:49:56 -0700

    end of class

--- week_3_homework_review.R
+++ week_3_homework_review.R
@@ -39,13 +39,30 @@ str(surveys)
 # Adding a new column
 # mutate: adds a new column
 surveys <- surveys %>%
-  mutate(weight_kg = weight/1000)
+  mutate(., weight_kg = weight/1000)
+str(surveys)
 
 # Add other columns
+surveys <- surveys %>%
+  mutate(., weight_kg = weight/1000,
+         weight_kg2 = weight_kg *2)
+str(surveys)
 
+surveys$weight_kg2 = as.character(surveys$weight_kg2)
+str(surveys)
 
 # Filter out the NA's
+ave_weight <- surveys %>% 
+  filter(!is.na(weight)) %>% 
+  mutate(mean_weight = mean(weight))
 
+str(ave_weight)
+nrow(ave_weight)
+ave_weight$mean_weight
+
+ave_weight <- surveys %>% 
+ filter(complete.cases(.))
+nrow(ave_weight)
 
 # Group_by and summarize ----
 # A lot of data manipulation requires us to split the data into groups, apply some analysis to each group, and then combine the results
@@ -57,20 +74,44 @@ surveys %>%
   mutate(mean_weight = mean(weight, na.rm = TRUE)) 
 
 
-# multiple groups
 surveys %>%
   group_by(sex, species_id) %>%
+ summarize(mean_weight = mean(weight, na.rm = TRUE))
+
+# multiple groups
+df <- surveys %>%
+  group_by(sex, species_id) %>%
   summarize(mean_weight = mean(weight, na.rm = TRUE))
+
+summary (df)
 
 
 # remove na's
 
 
 # sort/arrange order a certain way
+surveys %>%
+  group_by(sex, species_id) %>%
+  filter(sex != "") %>% 
+  summarize(mean_weight = mean(weight, na.rm = TRUE)) %>% 
+  arrange()
+
+surveys %>%
+  group_by(sex, species_id) %>%
+  filter(sex != "") %>% 
+  summarize(mean_weight = mean(weight, na.rm = TRUE)) %>% 
+  arrange(mean_weight) # ascending
+  
+surveys %>%
+  group_by(sex, species_id) %>%
+  filter(sex != "") %>% 
+  summarize(mean_weight = mean(weight, na.rm = TRUE)) %>% 
+  arrange(-mean_weight) # descending
 
 
 # Challenge
-#What was the weight of the heaviest animal measured in each year? Return a table with three columns: year, weight of the heaviest animal in grams, and weight in kilograms, arranged (arrange()) in descending order, from heaviest to lightest. (This table should have 26 rows, one for each year)
+#What was the weight of the heaviest animal measured in each year? 
+# Return a table with three columns: year, weight of the heaviest animal in grams, and weight in kilograms, arranged (arrange()) in descending order, from heaviest to lightest. (This table should have 26 rows, one for each year)
 
 
 #Try out a new function, count(). Group the data by sex and pipe the grouped data into the count() function. How could you get the same result using group_by() and summarize()? Hint: see ?n.
