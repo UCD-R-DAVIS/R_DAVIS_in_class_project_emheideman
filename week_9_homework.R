bb77766aca9e4fac54a9bd04f2d6d93767285661
Emily Heideman <emilyheideman@Emilys-MacBook-Pro-109.local>
2024-12-12 08:52:10 -0800

    upload

---
+++ week_9_homework.R
@@ -0,0 +1,10 @@
+surveys <- read.csv("data/portal_data_joined.csv")
+mloa <- read_csv("https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
+
+for(i in unique(surveys$taxa)){
+  mytaxon <- surveys[surveys$taxa == i,]
+  longestnames <- mytaxon[nchar(mytaxon$species) == max(nchar(mytaxon$species)),] 
+  %>% select(species)
+  print(paste0("The longest species name(s) among ", i, "s is/are: "))
+  print(unique(longestnames$species))
+}
